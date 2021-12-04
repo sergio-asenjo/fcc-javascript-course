d5d72b72d714b388089d64f9e6eb0515e6c38df2
Sergio Asenjo <sergio.asenjo@usach.cl>
2021-12-03 23:18:43 -0300

    add debugging folder and first lesson

---
+++ Debugging/1.js
@@ -0,0 +1,9 @@
+// ----------------------
+// Use the JavaScript Console to Check the Value of a Variable
+// ----------------------
+
+let a = 5;
+let b = 1;
+a++;
+let sumAB = a + b;
+console.log(a);
